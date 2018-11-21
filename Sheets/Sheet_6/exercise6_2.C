3e302abf0997ea6a9a8cb949f7f8c515da98cadd
Maurice Morgenthaler <43138263+MMorgenthaler@users.noreply.github.com>
2018-11-21 12:27:47 +0100

    Add files via upload
    Added part c) and d) but seem buggy

--- Sheets/Sheet_6/exercise6_2.C
+++ Sheets/Sheet_6/exercise6_2.C
@@ -11,6 +11,8 @@
 #include <vector>
 using namespace std;
 
+int nBins; //forgive me father for I have sinned 
+
 vector<double> dataGenerator (int nData) {
 	vector<double> data;
 	TRandom3* rndm = new TRandom3();
@@ -18,8 +20,8 @@ vector<double> dataGenerator (int nData) {
 	
 	return data;
 }
-
-double NLL(vector<double> x, double *param){
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+double unbin_NLL(vector<double> x, double *param){
 	double tau  = param[0];
 	double l = 0;
 	int nData = 100;
@@ -31,24 +33,52 @@ double NLL(vector<double> x, double *param){
 	return -l;
 }
 
-void fcn(int & npar, double * gin, double & f, double * par, int iflag){
-	f = NLL(dataGenerator(100), par);
+void fcn_unbinned(int & npar, double * gin, double & f, double * par, int iflag){
+	f = unbin_NLL(dataGenerator(100), par);
 	return;
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+double bin_ext_NLL(vector<double> x, double *param){
+	double l = 0;
+	int nData = x.size();
+	double upper = 4;
+	double lower = 0;
+	//nBins = 8;//(upper-lower)*2;
+	vector<double> nu;
+	vector<double> bin;
+	TH1D* h = new TH1D("histo", "histo", nBins, lower, upper);
+	for (unsigned i=1; i<=nData; i++) h->Fill(x[i]);
+	for (unsigned i=1; i<=nBins; i++) {
+		nu.push_back(nData*h->Integral(h->GetBinLowEdge(i), h->GetBinLowEdge(i+1)));
+		bin.push_back(h->GetBinContent(i));
+	}
 
-static std::vector<double> fitParam(){
+	for(unsigned j=0; j<nBins; j++){
+		l += bin[j]*TMath::Log(nu[j]) - nu[j];
+	}
+	
+	delete h;
+	// minus sign because of NLL
+	return -l;
+}
+
+void fcn_binned(int & npar, double * gin, double & f, double * par, int iflag){
+	f = bin_ext_NLL(dataGenerator(100), par);
+	return;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+static vector<double> fitParam(bool binned, int bins=1){
+	nBins = bins;
 	TMinuit minuit;
-	minuit.SetFCN(fcn);
-	minuit.SetErrorDef(0.5);
+	if (binned) minuit.SetFCN(fcn_binned);
+	else minuit.SetFCN(fcn_unbinned);
 	minuit.DefineParameter(0, "tau", 1., 0.001, 0.01, 3);
+	minuit.SetErrorDef(0.5);
 	minuit.Migrad();
 	minuit.Print();
 	
 	double tau_fit, tau_fit_error;
 	minuit.GetParameter(0, tau_fit, tau_fit_error);
-
-	cout << "Part b)" << endl;
-	std::cout << "Tau = "  << tau_fit << std::endl;
 
 	std::vector<double> result;
 	result.push_back(tau_fit);
@@ -64,39 +94,20 @@ double Decay(double* x, double* param){
 	return f;
 }
 
+
 void exercise6_2() {
-	TH1D* h = new TH1D("histo", "histo", 30, 0, 3);
-	TCanvas* c = new TCanvas("c", "c");
-	int nData = 100;
-	vector<double> data = dataGenerator(nData);
-	std::vector<double> fit_values = fitParam();
+	double tau_unbin = fitParam(false)[0];
+	double tau_8_bin = fitParam(true, 8)[0];
+	double tau_1_bin = fitParam(true, 1)[0];
+	double tau_1000_bin = fitParam(true, 1000)[0];
 	
-	// Draw Histogram
-	for(unsigned i = 0; i < nData; i++){
-		h->Fill(data[i]);
-	}
-	h->SetTitle("Distribution of measurements");
-	h->SetXTitle("Value [/]");
-	h->SetYTitle("Counts [/]");
-	h->SetFillColor(kGreen);
-	h->SetStats(false);
-	h->Draw();
-
-	// Draw gaussian with fixed sigma
-	TF1* decay = new TF1("f", Decay, 0.001, 3, 1);
-	decay->SetParameter(0, fit_values[0]);
-	decay->SetLineColor(1);
-	decay->Draw("same");
-
-	// Draw legend
-	TLegend * legend = new TLegend(0.6, 0.75, 0.9, 0.9);
-	legend->SetHeader("Legend", "C");
-	legend->AddEntry(h, "data", "f");
-	legend->AddEntry(decay, "decay law", "l");
-	legend->Draw("same");
-
-	// Save figure
-	TImage* img = TImage::Create();
-	img->FromPad(c);
-	img->WriteImage("exercise6_2.png");
+	cout << "part b)" << endl
+		  << " tau_unbinned: " << tau_unbin << endl << endl;
+	  
+	cout << "part c)" << endl;
+	cout << " tau_binned for delta t = 0.5s: " << tau_8_bin << endl;
+	
+	cout << "part d)" << endl;
+	cout << " tau_binned for delta t -> inf: " << tau_1_bin << endl;
+	cout << " tau_binned for delta t -> 0: " << tau_1000_bin << endl;
 }
