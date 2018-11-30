012a6ab43f04c66ff327ac3b5520b987bd44ef73
Nils Hoyer <42209925+nhoyer96@users.noreply.github.com>
2018-11-30 16:20:45 +0100

    Add files via upload

--- Sheets/Sheet_7/exercise7_2.C
+++ Sheets/Sheet_7/exercise7_2.C
@@ -7,7 +7,47 @@
 #include <TCanvas.h>
 #include <TMultiGraph.h>
 #include <TLegend.h>
+#include <TMinuit.h>
+#include <TMath.h>
+#include <TF1.h>
 
+Double_t Movement(Double_t * x, Double_t * par){
+  Double_t x_ = x[0];
+  Double_t p_ = par[0]
+  Double_t q_ = par[1]; // value here
+  Double_t B_ = par[2]; // value here
+  return TMath::Sqrt(TMath::Power(p_ / (q_ * B_), 2) - TMath::Power(x_, 2));
+}
+
+
+
+void fcn(Int_t & npar, Double_t * gin, Double_t & f, Double_t * par, Int_t iflag)
+{
+  Double_t chi2 = 0.0;
+  Double_t delta = 0.0;
+  Double_t sigma_x = 0.0;
+  Double_t sigma_z = 0.0002;
+
+  Int_t n = 10;
+/*
+  for(unsigned i = 0; i < n; i++){
+    delta = 1.0 / sigma_z *
+  }
+
+
+   const Int_t nbins = 5;
+   Int_t i;
+
+   //calculate chisquare
+   Double_t chisq = 0;
+   Double_t delta;
+   for (i=0;i<nbins; i++) {
+     delta  = (z[i]-func(x[i],y[i],par))/errorz[i];
+     chisq += delta*delta;
+   }
+   f = chisq;
+   */
+}
 void exercise7_2(){
   // Open file and save input of tree
   TFile * ifile = new TFile("exercise07.root", "read");
@@ -42,21 +82,29 @@ void exercise7_2(){
   // Plot first five tracks
   TCanvas * canvas = new TCanvas("canvas", "canvas", 10, 10, 900, 600);
   TMultiGraph * multigraph = new TMultiGraph();
-  TLegend * legend = new TLegend(0.15, 0.2, 0.53, 0.4);
+  TLegend * legend = new TLegend(0.15, 0.2, 0.33, 0.4);
   TGraph * g[5];
 
   legend->SetHeader("Legend", "C");
 
   for(unsigned i = 0; i < 5; i++){
-    g[i] = new TGraph(M, x[i], z[i]);
+    g[i] = new TGraph(M, z[i], x[i]);
     g[i]->SetLineColor(i + 40);
     TString name = "track no. " + std::to_string(i+1);
     legend->AddEntry(g[i], name, "l");
     multigraph->Add(g[i], "lp*");
   }
   multigraph->Draw("a");
+  multigraph->SetTitle("First five tracks");
+  multigraph->GetXaxis()->SetTitle("z value");
+  multigraph->GetYaxis()->SetTitle("x value");
   legend->Draw("same");
   canvas->SaveAs("./tracks.png");
+
+  // Uncertainties for x and z direction.
+  double sigma_x = 0.0;
+  double sigma_z = 0.0002;
+
 
   exit(1);
   return;
