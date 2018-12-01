b082673375b2c1f06ae0a6f55f58a6a24dadff6a
Nils Hoyer <42209925+nhoyer96@users.noreply.github.com>
2018-12-01 16:32:14 +0100

    Problem with error
    Currently there is a problem with finding the error leading to 'nan' and '-nan' for the fit of v.
    Problem is coming from the Gamma-function but I was unable to find a solution yet.

--- Sheets/Sheet_7/exercise7_2.C
+++ Sheets/Sheet_7/exercise7_2.C
@@ -10,16 +10,26 @@
 #include <TMinuit.h>
 #include <TMath.h>
 #include <TF1.h>
+#include <TH1D.h>
 
-Double_t Movement(Double_t * x, Double_t * par){
-  Double_t x_ = x[0];
-  Double_t p_ = par[0]
-  Double_t q_ = par[1]; // value here
-  Double_t B_ = par[2]; // value here
-  return TMath::Sqrt(TMath::Power(p_ / (q_ * B_), 2) - TMath::Power(x_, 2));
+Int_t n[5000], val = 0;
+Double_t x[5000][10], z[5000][10];
+
+Double_t Gamma(Double_t & v){
+  Double_t c = 2.99; // 10⁻⁸ m s⁻¹
+  v /= 1e8; // 10⁻⁸ m s⁻¹
+  return 1.0 / TMath::Sqrt(1 - TMath::Power((v / c), 2));
 }
 
-
+Double_t Movement(Double_t & x_, Double_t * par){
+  Double_t v_ = par[0]; // Kg m s⁻¹
+  Double_t O_ = par[1]; // Offset from zero
+  Double_t q_ = 1.602e-19; // C
+  Double_t B_ = 0.1; // T
+  Double_t me_ = 9.11e-31; // Kg
+  Double_t c = 2.99e8; // m s⁻¹
+  return TMath::Sqrt(TMath::Power(me_ * v_ * Gamma(v_)/ (c * B_ * q_), 2) - TMath::Power(x_, 2)) - O_;
+}
 
 void fcn(Int_t & npar, Double_t * gin, Double_t & f, Double_t * par, Int_t iflag)
 {
@@ -28,34 +38,20 @@ void fcn(Int_t & npar, Double_t * gin, Double_t & f, Double_t * par, Int_t iflag
   Double_t sigma_x = 0.0;
   Double_t sigma_z = 0.0002;
 
-  Int_t n = 10;
-/*
-  for(unsigned i = 0; i < n; i++){
-    delta = 1.0 / sigma_z *
+  for(unsigned i = 0; i < 10; i++){
+    delta = (x[val][i] - Movement(z[val][i], par)) / sigma_z;
+    chi2 += TMath::Power(delta, 2);
   }
-
-
-   const Int_t nbins = 5;
-   Int_t i;
-
-   //calculate chisquare
-   Double_t chisq = 0;
-   Double_t delta;
-   for (i=0;i<nbins; i++) {
-     delta  = (z[i]-func(x[i],y[i],par))/errorz[i];
-     chisq += delta*delta;
-   }
-   f = chisq;
-   */
+  f = chi2;
 }
+
 void exercise7_2(){
   // Open file and save input of tree
   TFile * ifile = new TFile("exercise07.root", "read");
   TTree * itree = (TTree *) ifile->Get("hits");
 
-  unsigned long N = itree->GetEntries();
   unsigned M = 10; // number of z val in tree
-
+  unsigned long N = itree->GetEntries();
 
   Int_t n_ = 0;
   TBranch * b_n_;
@@ -66,9 +62,6 @@ void exercise7_2(){
   Double_t z_[M]; // in meter
   TBranch * b_z_;
   itree->SetBranchAddress("z", & z_, & b_z_);
-
-  Int_t n[N];
-  Double_t x[N][M], z[N][M];
 
   for(unsigned i = 0; i < N; i++){
     itree->GetEntry(i);
@@ -96,8 +89,8 @@ void exercise7_2(){
   }
   multigraph->Draw("a");
   multigraph->SetTitle("First five tracks");
-  multigraph->GetXaxis()->SetTitle("z value");
-  multigraph->GetYaxis()->SetTitle("x value");
+  multigraph->GetXaxis()->SetTitle("z value in [m]");
+  multigraph->GetYaxis()->SetTitle("x value in [m]");
   legend->Draw("same");
   canvas->SaveAs("./tracks.png");
 
@@ -105,6 +98,27 @@ void exercise7_2(){
   double sigma_x = 0.0;
   double sigma_z = 0.0002;
 
+  // Fitting the model to the data
+  TMinuit minuit;
+  // suppress ouput printing
+  minuit.SetPrintLevel(-1);
+
+  // save momentum, kin energy values and their errors in array
+  Double_t velo_[N];
+
+  for(val; val < N; val++){
+    minuit.SetFCN(fcn);
+    minuit.SetErrorDef(0.5);
+    minuit.DefineParameter(0, "Momentum", 1e19, 0.001, 0, 0);
+    minuit.DefineParameter(1, "Offset", -0.02, 0.001, 0, 0);
+    minuit.Migrad();
+
+    Double_t velocity, velocity_err;
+    minuit.GetParameter(0, velocity, velocity_err);
+
+    velo_[val] = velocity;
+  }
+  std::cout << velo_[0] << std::endl;
 
   exit(1);
   return;
