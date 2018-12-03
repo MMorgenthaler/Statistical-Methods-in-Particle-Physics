0811d1200d81e8b870937134ef36170efe7e492c
Maurice Morgenthaler <43138263+MMorgenthaler@users.noreply.github.com>
2018-12-03 19:08:10 +0100

    integral doesn't converge

--- Sheets/Sheet_7/exercise7_1.C
+++ Sheets/Sheet_7/exercise7_1.C
@@ -5,6 +5,14 @@
 
 int Ndata = 500;
 
+Double_t pdfA(Double_t *val, Double_t *par) {
+	return exp(-val[0])*(1+0.3*sin(0.7*val[0]));
+};
+
+Double_t pdfB(Double_t *val, Double_t *par) {
+	return exp(-val[0])*(1-0.3*sin(0.7*val[0]));
+};
+
 Double_t ShannonA(Double_t *val, Double_t *par) {
 	double lambda = val[0];
 	double lLdev = 0; 
@@ -38,35 +46,33 @@ Double_t ShannonC(Double_t *val, Double_t *par) {
 
 
 void exercise7_1() {
-	//Data muss noch generiert werden
-	/*
-	RooRealVar t("t", "t", 0, 10);
-	RooRealVar lambda("lambda", "lambda", 0.3);
-	RooGenericPdf pdfA("pdfA", "pdfA", "exp(-t)*(1+lambda*sin(0.7*t)", RooArgSet(t, lambda));
-	RooGenericPdf pdfB("pdfB", "pdfB", "exp(-t)*(1-lambda*sin(0.7*t)", RooArgSet(t, lambda));
-	RooDataSet* datasetA = pdfA.generate(t, Ndata);
-	RooDataSet* datasetB = pdfA.generate(t, Ndata);
-	datasetA->Print("v");
-	//double dataA = datasetA->get(0)->find("t")->GetVal();
-	*/
+	double inf = std::numeric_limits<double>::infinity();
 	
-	TF1* toBeIntA = new TF1("toBeIntA", ShannonA, 5., 15., Ndata);
-	TF1* toBeIntB = new TF1("toBeIntB", ShannonB, 5., 15., Ndata);
-	TF1* toBeIntC = new TF1("toBeIntC", ShannonC, 5., 15., Ndata);
+	TF1* toBeIntA = new TF1("toBeIntA", ShannonA, 0., 10., Ndata);
+	TF1* toBeIntB = new TF1("toBeIntB", ShannonB, 0., 10., Ndata);
+	TF1* toBeIntC = new TF1("toBeIntC", ShannonC, 0., 10., Ndata);
+	
+	TF1* fA = new TF1("fA",pdfA,0.,10.);
+	TF1* fB = new TF1("fB",pdfB,0.,10.);
+	
+	double dataA[Ndata], dataB[Ndata];
 	
 	for(int i=0; i<Ndata; i++) {
+		dataA[i] = fA->GetRandom();
 		toBeIntA->SetParameter(i, dataA[i]);
+		
+		dataB[i] = fB->GetRandom();
 		toBeIntB->SetParameter(i, dataB[i]);
 	}
-	
+
 	for(int j=0; j<Ndata/2; j++) {
 		toBeIntC->SetParameter(j, dataA[j]);
 		toBeIntC->SetParameter(j+Ndata/2, dataB[j]);	
 	}
 
-	double uncerA = 1./sqrt(toBeIntA->Integral(-TMath::infinity(), TMath::infinity())); 
-	double uncerB = 1./sqrt(toBeIntB->Integral(-TMath::infinity(), TMath::infinity())); 
-	double uncerC = 1./sqrt(toBeIntC->Integral(-TMath::infinity(), TMath::infinity())); 
+	double uncerA = 1./sqrt(toBeIntA->Integral(0., 1000)); 
+	double uncerB = 1./sqrt(toBeIntB->Integral(0., 1000)); 
+	double uncerC = 1./sqrt(toBeIntC->Integral(0., 1000)); 
 	
 	
 	std::cout << "Uncertainty a: " << uncerA << std::endl;
